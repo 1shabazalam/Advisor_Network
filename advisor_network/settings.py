4dd24ec58e16a8d2d707ca89208079a909673416
crimemanagement <all4team.indira@gmail.com>
2021-05-18 23:54:36 +0530

    settings

--- advisor_network/settings.py
+++ advisor_network/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'advisor_network.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'advisor_network',
-#         'USER' : 'root',
-#         'PASSWORD' : '',
-#         'HOST' : 'localhost',
-#         'PORT' : '3306'
-#     } 
-# }
-
 DATABASES = {
-    'default' : {
-        'ENGINES' : 'django.db.backends.sqlite3',
-        'NAME' : (BASE_DIR/'db.sqlite3'),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'advisor_network',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
+    } 
 }
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINES' : 'django.db.backends.sqlite3',
+#         'NAME' : (BASE_DIR/'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
